48a61c5e51145baa89389042e9fbda295cfbb66c
Neethu Santhosh <v-nsanthosh@expedia.com>
2020-06-11 18:10:06 +0530

    project restructuring

--- app/src/App.js
+++ app/src/App.js
@@ -5,24 +5,26 @@ import './App.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.js';
-import  MainFormComponent from './MainFormComponent';
-function App() {
-  return (
-    <div className="App">
-      <div className="container-fluid">
-      <div className="row bg-white hdr-border">
-        <div className="col-12">
-          <header className="py-4 app-header">
-            <h1>CDX GOD(E)</h1>
-          </header>
+import MainFormComponent from './MainFormComponent';
+import HomePage from './views/home-page.js'
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="container-fluid">
+                <div className="row bg-white hdr-border">
+                    <div className="col-12">
+                        <header className="py-4 app-header">
+                            <h1>CDX GOD(E)</h1>
+                        </header>
+                    </div>
+                </div>
+                <div className="container-lg py-4 my-3 bg-white border rounded">
+                    <HomePage></HomePage>
+                </div>
+            </div>
         </div>
-      </div>
-        <div className="container-lg py-4 my-3 bg-white border rounded">
-          <MainFormComponent></MainFormComponent>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ app/src/views/home-page.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import Notifications from "react-notify-toast";
+
+export default class HomePage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Notifications/>
+                <div id="accordion">
+                    <div className="card">
+                        <div className="card-header" id="headingOne">
+                            <h5 className="mb-0">
+                                <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                    Entity Design
+                                </button>
+                            </h5>
+                        </div>
+
+                        <div id="collapseOne" className="collapse show" aria-labelledby="headingOne"
+                             data-parent="#accordion">
+                            <div className="card-body">
+                                <form>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-header" id="headingTwo">
+                            <h5 className="mb-0">
+                                <button className="btn btn-link collapsed" data-toggle="collapse"
+                                        data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    App Design
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo"
+                             data-parent="#accordion">
+                            <div className="card-body">
+                                <form>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-header" id="headingThree">
+                            <h5 className="mb-0">
+                                <button className="btn btn-link collapsed" data-toggle="collapse"
+                                        data-target="#collapseThree" aria-expanded="false"
+                                        aria-controls="collapseThree">
+                                    App Build
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseThree" className="collapse" aria-labelledby="headingThree"
+                             data-parent="#accordion">
+                            <div className="card-body">
+                                <form>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-header" id="headingFour">
+                        <h5 className="mb-0">
+                            <button className="btn btn-link collapsed" data-toggle="collapse"
+                                    data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                App Deploy
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseFour" className="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                        <div className="card-body">
+                            <form>
+
+                            </form>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-header" id="headingFive">
+                            <h5 className="mb-0">
+                                <button className="btn btn-link collapsed" data-toggle="collapse"
+                                        data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                    API Doc & Admin Portal
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseFive" className="collapse" aria-labelledby="headingFive"
+                             data-parent="#accordion">
+                            <div className="card-body">
+                                <div>
+                                    <button type="button" className="btn btn-primary"
+                                            onClick={(e) => this.handleLinkClick('portal')}>Link to Admin Portal
+                                    </button>
+
+                                    <button type="button" className="btn btn-primary ml-5"
+                                            onClick={(e) => this.handleLinkClick('docs')}>Link to API Docs
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+
+}
