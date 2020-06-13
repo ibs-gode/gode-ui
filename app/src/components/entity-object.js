d0df65702eaaa2501c8d15b75c4995ec0d39609a
Neethu Santhosh <v-nsanthosh@expedia.com>
2020-06-13 15:35:20 +0530

    - Custom hook to read value from field - Adding fields in Object functionality

--- app/src/components/entity-object.js
+++ app/src/components/entity-object.js
@@ -1,67 +1,97 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useInput} from "../hooks/input-hook";
 
 const EntityObject = () => {
 
-        return (
-            <fieldset className="form-group border border-secondary pl-3 pt-3 pr-3 mr-3 rounded"
-                      id="type-object">
+    const [entityItems, setEntityItems] = useState([]);
+    const {value: fieldName, bind: bindFieldName, reset: resetFieldName} = useInput('');
+    const {value: fieldDesc, bind: bindFieldDesc, reset: resetFieldDesc} = useInput('');
+    const {value: fieldType, bind: bindFieldType, reset: resetFieldType} = useInput('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+
+        setEntityItems([...entityItems, {
+            name: fieldName,
+            desc: fieldDesc,
+            type: fieldType
+        }]);
+        resetFieldName();
+        resetFieldDesc();
+        resetFieldType();
+    }
+    return (
+        <fieldset className="form-group border border-secondary pl-3 pt-3 pr-3 mr-3 rounded"
+                  id="type-object">
+            <div>
+                <div className="form-row ">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="entity-name">Name</label>
+                        <input type="text" className="form-control" id="entity-name"
+                               placeholder="Entity Name"/>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="entity-description">Description</label>
+                        <input type="text" className="form-control" id="entity-description"
+                               placeholder="Entity Description"/>
+                    </div>
+                </div>
                 <div>
-                    <div className="form-row ">
-                        <div className="form-group col-md-6">
-                            <label htmlFor="entity-name">Name</label>
-                            <input type="text" className="form-control" id="entity-name"
-                                   placeholder="Entity Name"/>
-                        </div>
-                        <div className="form-group col-md-6">
-                            <label htmlFor="entity-description">Description</label>
-                            <input type="text" className="form-control" id="entity-description"
-                                   placeholder="Entity Description"/>
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="type-object">Field Details</label>
-                        <fieldset className="form-group border  pl-3 pt-3 pr-3 mr-3 mt-2 rounded"
-                                  id="type-object">
-                                <input type="text" className="form-control-sm ml-5 mr-5 border"
-                                       id="field-name"
-                                       placeholder="Field Name"/>
-                                <input type="text" className="form-control-sm ml-5 mr-5 border"
-                                       id="field-description"
-                                       placeholder="Field Description"/>
-                                <select className="form-control-sm ml-5 mr-5 border" id="field-type">
-                                    <option value="" selected hidden>Type</option>
-                                    <option value="TEXT">Text</option>
-                                    <option value="OBJECT">Object</option>
-                                    <option value="BOOLEAN">Boolean</option>
-                                    <option value="DATE">Date</option>
-                                    <option value="RELATIONSHIP">Relationship</option>
-                                    <option value="LONG_TEXT">Long Text</option>
-                                    <option value="NUMBER">Number</option>
-                                    <option value="HIGH_PRECISION_NUMBER">High Precision Number</option>
-                                    <option value="DECIMAL">Decimal</option>
-                                    <option value="HIGH_PRECISION_DECIMAL">High Precision Decimal
-                                    </option>
-                                </select>
-                                <button type="button" className="btn btn-dark btn-sm ml-3" >+</button>
+                    <label htmlFor="type-object">Field Details</label>
+                    <fieldset className="form-group border  pl-3 pt-3 pr-3 mr-3 mt-2 rounded"
+                              id="type-object">
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" className="form-control-sm ml-5 mr-5 border"
+                                   id="field-name"
+                                   placeholder="Field Name" {...bindFieldName}/>
+                            <input type="text" className="form-control-sm ml-5 mr-5 border"
+                                   id="field-description"
+                                   placeholder="Field Description" {...bindFieldDesc}/>
+                            <select className="form-control-sm ml-5 mr-5 border" id="field-type" {...bindFieldType}>
+                                <option value="" selected hidden>Type</option>
+                                <option value="TEXT">Text</option>
+                                <option value="OBJECT">Object</option>
+                                <option value="BOOLEAN">Boolean</option>
+                                <option value="DATE">Date</option>
+                                <option value="RELATIONSHIP">Relationship</option>
+                                <option value="LONG_TEXT">Long Text</option>
+                                <option value="NUMBER">Number</option>
+                                <option value="HIGH_PRECISION_NUMBER">High Precision Number</option>
+                                <option value="DECIMAL">Decimal</option>
+                                <option value="HIGH_PRECISION_DECIMAL">High Precision Decimal
+                                </option>
+                            </select>
+                            <button type="submit" className="btn btn-dark btn-sm ml-3">+</button>
+                        </form>
 
-                            <table className="table table-striped mt-3 form-control-sm" >
-                                <thead>
-                                <tr>
-                                    <th className="font-weight-light">Name</th>
-                                    <th className="font-weight-light">Description</th>
-                                    <th className="font-weight-light">Type</th>
-                                </tr>
-                                </thead>
-                                <tbody></tbody>
-                            </table>
-                        </fieldset>
-                    </div>
-
-
+                        <table className="table table-striped mt-3 form-control-sm">
+                            <thead>
+                            <tr>
+                                <th className="font-weight-light">Name</th>
+                                <th className="font-weight-light">Description</th>
+                                <th className="font-weight-light">Type</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {entityItems.map((item) => {
+                                return (
+                                    <tr>
+                                        <td>{item.name}</td>
+                                        <td>{item.desc}</td>
+                                        <td>{item.type}</td>
+                                    </tr>
+                                )
+                            })}
+                            </tbody>
+                        </table>
+                    </fieldset>
                 </div>
 
-            </fieldset>
-        );
-    };
+
+            </div>
+
+        </fieldset>
+    );
+};
 
 export default EntityObject;
